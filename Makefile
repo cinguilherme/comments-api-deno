b4fd1702420cab0a71098793a4adf8765fa4bcd3
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-06-16 20:18:53 -0300

    added basic docker infrastructure to have a simple DB and the API up

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 run:
 	deno run  --allow-net --allow-read  src/app.ts
+
+build-image:
+	docker build . -t comments-deno-api:latest
--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,8 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 const HOST = config().HOST ?? "127.0.0.1";
-const PORT = config().PORT ?? 8000;
+const PORT = config().PORT ?? 3000;
 
 console.log(`deno is running on ${HOST} ${PORT}`);
-
-const host = app.use((ctx) => {
-  ctx.response.body = "Hello";
-});
 
 await app.listen(`${HOST}:${PORT}`);
